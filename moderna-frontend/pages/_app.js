000150f97039ab82d9b4214872036df292aeedab
171443 <kelvinngugi424@gmail.comsudo aptgit config --global user.email kelvinngugi424@gmail.com>
2025-03-19 11:54:03 +0000

    Remove pages directory for App Router

--- moderna-frontend/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import ErrorBoundary from "../components/ErrorBoundary";
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
-  );
-}
